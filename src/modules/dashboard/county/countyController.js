591cf3f451b9bd4d0c8d421763e0afd790e8b533
Burak Alparslan <Burak.Alparslan@monitise.com>
2017-02-20 16:53:13 +0300

    County page updated

--- src/modules/dashboard/county/countyController.js
+++ src/modules/dashboard/county/countyController.js
@@ -4,14 +4,42 @@ angular.module("store.county").controller("countyController",
             "use strict";
             var self = this;
             self.countyList = [];
+            self.countyId = "";
+            self.countyName = "a";
 
             self.successGetCounty = function (data) {
-              debugger;
               self.countyList = data;
             }
 
-            self.errorGetCounty = function (data) {
+            self.successSaveCounty = function (data) {
+              self.getCountyList();
+            }
 
+            self.errorCounty = function (data) {
+
+            }
+
+            self.setCounty = function (county) {
+              self.countyId = county.id;
+              self.countyName = county.name;
+            }
+
+            self.saveCounty = function () {
+              var data = {
+                name: self.countyName
+              };
+              var request = {
+                  name : "store",
+                  method: "PUT",
+                  url: "http://localhost:8080/secure/county",//TODO collect urls to constants
+                  data: data
+              };
+
+              var config = {
+                  "onSuccess": self.successSaveCounty,
+                  "onError": self.errorCounty
+              };
+              storeApi.makeRequest(request, config);
             }
 
             self.getCountyList = function () {
@@ -23,7 +51,7 @@ angular.module("store.county").controller("countyController",
 
                 var config = {
                     "onSuccess": self.successGetCounty,
-                    "onError": self.errorGetCounty
+                    "onError": self.errorCounty
                 };
                 storeApi.makeRequest(request, config);
 
