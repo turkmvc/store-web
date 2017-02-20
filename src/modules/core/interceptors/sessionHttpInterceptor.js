e4af66461d643ba0860141911e95400d2d5830f0
Burak Alparslan <Burak.Alparslan@monitise.com>
2017-02-20 15:44:41 +0300

    Added http interceptors

---
+++ src/modules/core/interceptors/sessionHttpInterceptor.js
@@ -0,0 +1,32 @@
+angular.module("store.core").factory("sessionHttpInterceptor",
+    ["$injector", "$q",
+        function ($injector, $q) {
+            "use strict";
+
+            return {
+                response: function (response) {
+                    return response;
+                },
+                responseError: function (response) {
+                    if (response.status === 401) {
+                        response.data = {};
+                        response.data.status = 401;
+                    }
+                    return $q.reject(response);
+                }
+            };
+        }
+    ]
+);
+
+angular.module("store.core").config(
+    ["$httpProvider",
+        function ($httpProvider) {
+            "use strict";
+            $httpProvider.defaults.useXDomain = true;
+            $httpProvider.defaults.withCredentials = true;
+
+            $httpProvider.interceptors.push("sessionHttpInterceptor");
+        }
+    ]
+);
